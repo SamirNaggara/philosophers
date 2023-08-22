24116de6173fa00eae666d10a2c409cbf06a2ad1
Sam <samirm.nagg@gmail.com>
2023-08-22 12:13:01 +0200

    fix: don't close log

--- end.c
+++ end.c
@@ -6,7 +6,7 @@
 /*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 14:21:43 by snaggara          #+#    #+#             */
-/*   Updated: 2023/06/27 22:15:57 by snaggara         ###   ########.fr       */
+/*   Updated: 2023/08/22 12:11:28 by snaggara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 int	ft_error_no_phil(t_data *data)
 {
 	printf(E_NO_PHIL);
-	close(data->fd_log);
 	return (0);
 }
 
@@ -30,14 +29,12 @@ int	ft_error_no_phil(t_data *data)
 */
 int	ft_bad_init(t_data *data)
 {
-	close(data->fd_log);
 	return (0);
 }
 
 int	ft_end_program(t_data *data)
 {
 	ft_destroy_philosophers(data);
-	close(data->fd_log);
 	pthread_mutex_destroy(&(data->write_mutex));
 	pthread_mutex_destroy(&(data->alive_mutex));
 	return (1);
